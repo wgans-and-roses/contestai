deae0ebdafe56478290f9a7f5eb160a77a8ce9cf
Marco <Marco@MBP-di-Marco.station>
2018-08-23 10:55:08 +0200

    Parse example

--- main.py
+++ main.py
@@ -6,6 +6,31 @@ import torchvision as tv
 from importlib import reload
 import numpy as np
 from custom_transforms import ToBand
+from util.parser import Parser
+args = \
+[
+[('--model', '-m'), {'type': str, 'default': 'lenet', 'help': 'Used Model lenet | allcnn |'}],
+[('--margin', '-mg'), {'type': float, 'default': 5.0, 'help': 'Margin used for the hinge loss'}],
+[('--dim',), {'type': int, 'default': 2, 'help': 'Dimension of the embedding'}],
+[('--lr',), {'type': float, 'default': 0.01, 'help': 'Learning rate'}],
+[('-d',), {'type': float, 'default': 1, 'help': 'Dropout probability'}],
+[('--dataset',), {'type': str, 'default': 'mnist', 'help': 'Dataset to use'}],
+[('--root',), {'type': str, 'default': '/mnt/DATA/TorchData', 'help': 'Location of the dataset'}],
+[('--save_path', '-s'), {'type': str, 'default': '/mnt/DATA/ProjectsResults/EllipticalEmb', 'help': 'Results path'}],
+[('--num_positive', '-np'), {'type': int, 'default': 1, 'help': 'Number of same class elements per training sample'}],
+[('--num_negative', '-nn'), {'type': int, 'default': 1, 'help': 'Number of different class elements per training sample'}],
+[('--batch_size', '-bs'), {'type': int, 'default': 128, 'help': 'Batch size'}],
+[('--epochs', '-e'), {'type': int, 'default': 30, 'help': 'Number of epochs'}],
+[('--categories', '-c'), {'type': int, 'nargs': '+', 'default': None, 'help': 'Categories used to train the model'}],
+[('--log_period', '-lp'), {'type': int, 'default': 30, 'help': 'Logging period in number of epochs'}],
+[('--num_iters', '-ni'), {'type': int, 'default': 20, 'help': 'Number of iterations for the matrix sqrt algorithm'}],
+[('--reg', ), {'type': float, 'default': 2.0, 'help': 'Regularization for the matrix sqrt algorithm'}],
+[('--reg2', ), {'type': float, 'default': 1E-8, 'help': 'Regularization for the gradient of the bures metric'}],
+]
+
+argparser = Parser("Deep Elliptical Embeddings")
+argparser.add_arguments(args)
+opt = argparser.get_dictionary()
 
 #%%
 W = 1280
